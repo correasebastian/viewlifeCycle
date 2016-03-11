e2ccd0df64586470e83eac0e9c03fd21a1d54c84
sebastian correa morales <correa.m.sebastian@gmail.com>
2016-03-11 16:15:28 -0500

    first commit

---
+++ www/js/controllers.js
@@ -0,0 +1,124 @@
+angular.module('starter.controllers', [])
+
+.controller('DashCtrl', function($scope) {
+
+    /* $scope.$on('$ionicView.loaded', function() {
+         console.log(' DashCtrl loaded', $scope, $scope.$id)
+     });
+
+     $scope.$on('$ionicView.beforeEnter', function() {
+         // Anything you can think of
+         console.log(' DashCtrl BeforeEnter', $scope, $scope.$id)
+     });
+     $scope.$on('$ionicView.enter', function() {
+         // Anything you can think of
+         console.log(' DashCtrl enter', $scope, $scope.$id)
+
+     });
+     $scope.$on('$ionicView.afterEnter', function() {
+         console.log(' DashCtrl afterEnter', $scope, $scope.$id)
+     });
+
+     $scope.$on('$ionicView.beforeLeave', function() {
+         console.log(' DashCtrl beforeLeave', $scope, $scope.$id)
+             // Anything you can think of
+     });
+
+     $scope.$on('$ionicView.leave', function() {
+         console.log(' DashCtrl Leave', $scope, $scope.$id)
+     });
+
+     $scope.$on('$ionicView.afterLeave', function() {
+         console.log(' DashCtrl afterLeave', $scope, $scope.$id)
+
+     });
+     $scope.$on('$ionicView.unloaded', function() {
+         console.log(' DashCtrl unloaded', $scope, $scope.$id)
+             // Anything you can think of
+     });*/
+
+})
+
+.controller('ChatsCtrl', function($scope, Chats) {
+    /*$scope.$on('$ionicView.loaded', function() {
+        console.log(' ChatsCtrl loaded', $scope, $scope.$id)
+    });
+    $scope.$on('$ionicView.beforeEnter', function() {
+        // Anything you can think of
+        console.log(' ChatsCtrl BeforeEnter', $scope, $scope.$id)
+    });
+    $scope.$on('$ionicView.enter', function() {
+        console.log(' ChatsCtrl enter', $scope, $scope.$id)
+            // Anything you can think of
+    });
+    $scope.$on('$ionicView.afterEnter', function() {
+        console.log(' ChatsCtrl afterEnter', $scope, $scope.$id)
+    });
+
+    $scope.$on('$ionicView.beforeLeave', function() {
+        console.log(' ChatsCtrl beforeLeave', $scope, $scope.$id)
+            // Anything you can think of
+    });
+    $scope.$on('$ionicView.leave', function() {
+        console.log(' ChatsCtrl Leave', $scope, $scope.$id)
+    });
+
+    $scope.$on('$ionicView.afterLeave', function() {
+        console.log(' ChatsCtrl afterLeave', $scope, $scope.$id)
+
+    });
+    $scope.$on('$ionicView.unloaded', function() {
+        console.log(' DashCtrl unloaded', $scope, $scope.$id)
+            // Anything you can think of
+    });
+*/
+
+    $scope.chats = Chats.all();
+    $scope.remove = function(chat) {
+        Chats.remove(chat);
+    };
+})
+
+.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+    var _str = 'ChatDetailCtrl'
+    $scope.chat = Chats.get($stateParams.chatId);
+   /* $scope.$on('$ionicView.loaded', function() {
+        console.log(_str + ' loaded', $scope, $scope.$id)
+    });
+    $scope.$on('$ionicView.beforeEnter', function() {
+        // Anything you can think of
+        console.log(_str + ' BeforeEnter', $scope, $scope.$id)
+    });
+    $scope.$on('$ionicView.enter', function() {
+        console.log(_str + ' enter', $scope, $scope.$id)
+            // Anything you can think of
+    });
+    $scope.$on('$ionicView.afterEnter', function() {
+        console.log(_str + ' afterEnter', $scope, $scope.$id)
+    });
+
+    $scope.$on('$ionicView.beforeLeave', function() {
+        console.log(_str + ' beforeLeave', $scope, $scope.$id)
+            // Anything you can think of
+    });
+    $scope.$on('$ionicView.leave', function() {
+        console.log(_str + ' Leave', $scope, $scope.$id)
+    });
+
+    $scope.$on('$ionicView.afterLeave', function() {
+        console.log(_str + ' afterLeave', $scope, $scope.$id)
+
+    });
+    $scope.$on('$ionicView.unloaded', function() {
+        console.log(_str + 'unloaded', $scope, $scope.$id)
+            // Anything you can think of
+    });*/
+})
+
+.controller('AccountCtrl', function() {
+
+    var vm = this;
+    vm.settings = {
+        enableFriends: true
+    };
+});
---
+++ www/js/services.js
@@ -0,0 +1,129 @@
+angular.module('starter.services', [])
+
+.factory('Chats', function() {
+    // Might use a resource here that returns a JSON array
+
+    // Some fake testing data
+    var chats = [{
+        id: 0,
+        name: 'Ben Sparrow',
+        lastText: 'You on your way?',
+        face: 'img/ben.png'
+    }, {
+        id: 1,
+        name: 'Max Lynx',
+        lastText: 'Hey, it\'s me',
+        face: 'img/max.png'
+    }, {
+        id: 2,
+        name: 'Adam Bradleyson',
+        lastText: 'I should buy a boat',
+        face: 'img/adam.jpg'
+    }, {
+        id: 3,
+        name: 'Perry Governor',
+        lastText: 'Look at my mukluks!',
+        face: 'img/perry.png'
+    }, {
+        id: 4,
+        name: 'Mike Harrington',
+        lastText: 'This is wicked good ice cream.',
+        face: 'img/mike.png'
+    }];
+
+    return {
+        all: function() {
+            return chats;
+        },
+        remove: function(chat) {
+            chats.splice(chats.indexOf(chat), 1);
+        },
+        get: function(chatId) {
+            for (var i = 0; i < chats.length; i++) {
+                if (chats[i].id === parseInt(chatId)) {
+                    return chats[i];
+                }
+            }
+            return null;
+        }
+    };
+});
+
+
+angular.module('starter.directives', [])
+    .directive('lifeCycle', lifeCycle);
+
+lifeCycle.$inject = ['$stateParams'];
+
+/* @ngInject */
+function lifeCycle($stateParams) {
+    // Usage:
+    //
+    // Creates:
+    //
+    var directive = {
+
+        link: link,
+        restrict: 'A',
+        scope: false
+    };
+    return directive;
+
+    function link(scope, element, attrs) {
+        console.time('loaded')
+        console.time('beforeEnter')
+        console.time('enter')
+        console.time('afterEnter')
+        console.time('beforeLeave')
+        console.time('leave')
+        console.time('afterLeave')
+        console.time('unloaded')
+        var _nameView = attrs.viewTitle;
+        console.log('lifeCycle directive', scope, scope.$id, _nameView, $stateParams);
+        scope.$on('$ionicView.loaded', function() {
+            console.log(_nameView + ' loaded', scope, scope.$id);
+            console.timeEnd('loaded');
+        });
+        scope.$on('$ionicView.beforeEnter', function() {
+            // Anything you can think of
+            console.log(_nameView + ' BeforeEnter', scope, scope.$id)
+            console.timeEnd('beforeEnter')
+            if (_nameView === 'Account') {
+                scope.vm.settings.name = _nameView;
+            }
+        });
+        scope.$on('$ionicView.enter', function() {
+            console.log(_nameView + ' enter', scope, scope.$id)
+            console.timeEnd('enter')
+                // Anything you can think of
+        });
+        scope.$on('$ionicView.afterEnter', function() {
+            console.log(_nameView + ' afterEnter', scope, scope.$id)
+            console.timeEnd('afterEnter');
+        });
+
+        scope.$on('$ionicView.beforeLeave', function() {
+            console.log(_nameView + ' beforeLeave', scope, scope.$id)
+            console.timeEnd('beforeLeave')
+
+            // Anything you can think of
+        });
+        scope.$on('$ionicView.leave', function() {
+            console.log(_nameView + ' Leave', scope, scope.$id)
+            console.timeEnd('leave')
+
+        });
+
+        scope.$on('$ionicView.afterLeave', function() {
+            console.log(_nameView + ' afterLeave', scope, scope.$id)
+            console.timeEnd('afterLeave')
+
+
+        });
+        scope.$on('$ionicView.unloaded', function() {
+            console.log(_nameView + 'unloaded', scope, scope.$id)
+            console.timeEnd('unloaded')
+                // Anything you can think of
+        });
+    }
+}
